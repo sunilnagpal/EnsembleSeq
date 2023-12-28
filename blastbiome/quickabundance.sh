12f9060ce695dc8d567e3e2801c281daea0e0e76
sunilnagpal <79209487+sunilnagpal@users.noreply.github.com>
2023-12-28 15:40:42 +0530

    Update quickabundance.sh

--- blastbiome/quickabundance.sh
+++ blastbiome/quickabundance.sh
@@ -1,7 +1,9 @@
 blastn -query FILENAME -db /path/BLASTDB/its/ITS_RefSeq_Fungi -outfmt "6 qseqid qlen length pident evalue sscinames sskingdoms staxids sblastnames scomnames" -max_target_seqs 1 -out BLASTFUN/FILENAME.blastout.txt -max_hsps 1
-mkdir countdata
-for blasout in `*.blastout.txt`
+mkdir species
+mkdir genus
+for blasout in `ls *.blastout.txt`
 do
 name=`echo $blasout | cut -d "." -f1,2`
-cat $blasout | cut -f6 | sort | uniq -c | awk '{print $2"_"$3"\t"$1}' > countdata/$name.count.txt
+awk '{if($4 >= 90) print $0}' $blasout | cut -f6 | sort | uniq -c | awk '{print $2"_"$3"\t"$1}' > species/$name.count.txt
+awk '{if($4 >= 90) print $0}' $blasout | cut -f6 | sort | uniq -c | awk '{print $2"\t"$1}' > genus/$name.count.txt
 done
