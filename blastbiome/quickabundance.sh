80c9a3cecf6454824379456b2df708666c8d243a
sunilnagpal <79209487+sunilnagpal@users.noreply.github.com>
2023-12-28 16:07:46 +0530

    Update quickabundance.sh

--- blastbiome/quickabundance.sh
+++ blastbiome/quickabundance.sh
@@ -5,5 +5,5 @@ for blasout in `ls *.blastout.txt`
 do
 name=`echo $blasout | cut -d "." -f1,2`
 awk '{if($4 >= 90) print $0}' $blasout | cut -f6 | sort | uniq -c | awk '{print $2"_"$3"\t"$1}' > species/$name.count.txt
-awk '{if($4 >= 90) print $0}' $blasout | cut -f6 | sort | uniq -c | awk '{print $2"\t"$1}' > genus/$name.count.txt
+awk '{if($4 >= 90) print $0}' $blasout | cut -f6 | awk '{print $1}' | sort | uniq -c | awk '{print $2"\t"$1}' > genus/$name.count.txt
 done
