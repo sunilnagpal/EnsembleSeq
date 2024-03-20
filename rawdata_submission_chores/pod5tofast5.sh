43f2cac3448286d26a82ec32829ffc8ebf13614b
sunilnagpal <79209487+sunilnagpal@users.noreply.github.com>
2024-03-20 11:07:28 +0530

    indentation

--- rawdata_submission_chores/pod5tofast5.sh
+++ rawdata_submission_chores/pod5tofast5.sh
@@ -1,6 +1,6 @@
 #NCBI doesn't support pod5 files hence converting those to fast5 format
 for files in `ls *.pod5`
 do
-name=`echo $files | cut -d "." -f1`
+  name=`echo $files | cut -d "." -f1`
   pod5 convert to_fast5 $files --file-read-count 1000000 --output $name.fast5 
 done
